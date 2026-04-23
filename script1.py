c5b8192924f05ad33b34aa36f2ff422684aa3263
c-hurson-rncat <christophe.hurson@reserves-naturelles-catalanes.org>
2026-04-23 15:08:33 +0200

    1507_ajout de script 1

---
+++ script1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Script: bonjour.py
+Auteur: Votre Nom
+Date: 23/04/2026
+Description: Ce script affiche un message de salutation.
+"""
+
+print("Bonjour !")
